58a94697d9cd794105b134978f974d3ca82a3d0d
justin <justin1726hernandez2@gmail.com>
2026-03-13 19:32:43 -0400

    including the boost asio library

--- src/PortScanner.hpp
+++ src/PortScanner.hpp
@@ -1,8 +1,14 @@
 #ifndef PORTSCANNER_HPP
 #define PORTSCANNER_HPP
 
-class Scanner {
- // TODO: add all scanner decleration here
+#include <boost/asio.hpp>
+class PortScanner {
+    private: 
+     
+
+    public:
+    
+
 };
 
 #endif
