9f966bbfbae7f4bb8e579713e8a120e71ea5690f
justin <justin1726hernandez2@gmail.com>
2026-03-14 11:16:43 -0400

    renaming variables

--- src/PortScanner.cpp
+++ src/PortScanner.cpp
@@ -9,20 +9,20 @@ void PortScanner::scan(std::string ip, int port) {
     boost::asio::io_context io;
 
     // variables to connect to tcp socket 
-    boost::asio::ip::tcp::endpoint end_point (boost::asio::ip::make_address(ip), port);
+    boost::asio::ip::tcp::endpoint endPointTcp (boost::asio::ip::make_address(ip), port);
     boost::asio::ip::tcp::socket tcpSocket (io);
 
     // variables to connect to a udp socket
-    boost::asio::ip::udp::endpoint end_point_udp (boost::asio::ip::make_address(ip), port);
+    boost::asio::ip::udp::endpoint endPointUdp (boost::asio::ip::make_address(ip), port);
     boost::asio::ip::udp::socket udpSocket (io);
 
     try {
-        tcpSocket.connect(end_point);
+        tcpSocket.connect(endPointTcp);
         std::cout << "Port " << port << " OPEN\n";
     }
     catch (boost::system::system_error& e) {
         closed_count ++;
-        // std::cout << "Port " << port << " CLOSED — " << e.what() << "\n";
+        //std::cout << "Port " << port << " CLOSED — " << e.what() << "\n";
     }
     tcpSocket.close();
 }
