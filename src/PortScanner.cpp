72683b6682c9a3b84f61e7389e7f1c3ae38518c2
justin <justin1726hernandez2@gmail.com>
2026-03-16 00:32:28 -0400

    trasition from ssync scanning to async

--- src/PortScanner.cpp
+++ src/PortScanner.cpp
@@ -1,15 +1,39 @@
 #include "PortScanner.hpp"
+#include <boost/asio/error.hpp>
+#include <boost/asio/io_context.hpp>
 #include <boost/asio/ip/address.hpp>
 #include <boost/asio/ip/udp.hpp>
 #include <iostream>
 #include <iomanip>
+#include <memory>
 #include <unordered_map>
 #include <ostream>
- // TODO: add all scanner logic here [DO NOT add user arguements here (cli responses)]
 
-void PortScanner::scan(std::string ip, int port) {
+ void PortScanner::startScan(std::string ip, int start_port, int end_port) {
+    // the engine
+    boost::asio::io_context io;
 
-    const std::unordered_map<int, std::string> portServices = {
+    // variables for the async scan; timer limit and currrent amount of 
+    int in_flight = 0;
+    
+    for (; start_port < end_port; start_port++) {
+        portScan(ip, start_port, io);
+        in_flight++;
+
+        if (in_flight == 100){ 
+            io.run();
+            io.restart();
+
+            in_flight = 0;
+        }
+    }
+    io.run();
+
+ }
+
+void PortScanner::portScan(std::string ip, int port, boost::asio::io_context &io) {
+
+    static const std::unordered_map<int, std::string> portServices = {
         {21,    "FTP"},
         {22,    "SSH"},
         {23,    "Telnet"},
@@ -33,31 +57,41 @@ void PortScanner::scan(std::string ip, int port) {
     auto it = portServices.find(port);
     std::string service = (it != portServices.end()) ? it->second : "Unknown";
 
-    // the main engine
-    boost::asio::io_context io;
-
     // variables to connect to tcp socket 
     boost::asio::ip::tcp::endpoint endPointTcp (boost::asio::ip::make_address(ip), port);
-    boost::asio::ip::tcp::socket tcpSocket (io);
+    auto pTcpSocket = std::make_shared<boost::asio::ip::tcp::socket>(io);
 
-    // variables to connect to a udp socket
-    boost::asio::ip::udp::endpoint endPointUdp (boost::asio::ip::make_address(ip), port);
-    boost::asio::ip::udp::socket udpSocket (io);
+    auto pTimer = std::make_shared<boost::asio::steady_timer> (io, std::chrono::seconds(2));
 
-    // checks the ports
-    try {
-        tcpSocket.connect(endPointTcp);
+    // callback
+    std::function<void(const boost::system::error_code& error)> callback = [this, port, service, pTcpSocket, endPointTcp, pTimer] 
+    (const boost::system::error_code& error) {
         
-        // In your scan output:
-        std::cout << std::left
-          << std::setw(10) << port       
-          << std::setw(10) << "OPEN"  
-          << std::setw(10) << service 
-          << std::endl;
-    }
-    catch (boost::system::system_error& e) {
-        closed_count ++;
-        //std::cout << "Port " << port << " CLOSED — " << e.what() << "\n";
-    }
-    tcpSocket.close();
+        // checks the ports
+        if (error.value() == 0) {
+            
+            // In your scan output:
+            std::cout << std::left
+            << std::setw(10) << port       
+            << std::setw(10) << "OPEN"  
+            << std::setw(10) << service 
+            << std::endl;
+        }
+        else if (error == boost::asio::error::operation_aborted) {
+            // In your scan output:
+            std::cout << std::left
+            << std::setw(10) << port       
+            << std::setw(10) << "FILTERED"  
+            << std::setw(10) << service 
+            << std::endl;
+
+        }
+        else {
+            this->closed_count ++;
+            //std::cout << "Port " << port << " CLOSED — " << "\n";
+        }
+    };
+
+    pTcpSocket->async_connect( endPointTcp, callback);
+
 }
